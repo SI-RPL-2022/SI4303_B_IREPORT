7e00245af90bb643937d47fac9939d0c43798195
alaric2001 <alaric2001ra@gmail.com>
2022-03-29 08:45:45 +0700

    Buat Project

---
+++ IREPORT/routes/web.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('master', function () {
+    return view('layouts/master');
+});
+
+Route::get('about', function () {
+    return view('about');
+});
+// user
+Route::get('detail', function () {
+    return view('user/detail');
+});
+Route::get('edit', function () {
+    return view('user/edit');
+});
+Route::get('home', function () {
+    return view('user/home');
+});
+Route::get('input', function () {
+    return view('user/input');
+});
+
+
+// admin
+// berita
+Route::get('createBerita', function () {
+    return view('admin/berita/create');
+});
+Route::get('indexBerita', function () {
+    return view('admin/berita/index');
+});
+Route::get('showBerita', function () {
+    return view('admin/berita/show');
+});
+
+// laporan
+Route::get('createLaporan', function () {
+    return view('admin/laporanUSer/create');
+});
+Route::get('indexLaporan', function () {
+    return view('admin/laporanUSer/index');
+});
+Route::get('showLaporan', function () {
+    return view('admin/laporanUSer/show');
+});
