9e32dd371b5650eec8f617cc11163baeb9b77bf1
alaric2001 <alaric2001ra@gmail.com>
2022-03-31 23:19:23 +0700

    tambah footer, edit page, dan table dari migration

--- IREPORT/resources/views/layouts/master.blade.php
+++ IREPORT/resources/views/layouts/master.blade.php
@@ -9,14 +9,49 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <title>Hello, world!</title>
-    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <meta content="" name="description">
+        <meta content="" name="keywords">
+
+        <link rel = "icon" type="image/x-icon" href="assets/favicon.ico" />
         <link rel = "shortcut icon" type = "image/png" href = "{{ asset('logoIREPORT.png') }}">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
-        <link href="{{ asset('template1/css/styles.css') }}" rel="stylesheet" />
+        {{-- <link rel = "stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css"> --}}
+        <link href = "{{ asset('template/assets/css/style.css') }}" rel="stylesheet" />
+
+        <!-- Favicons -->
+        {{-- <link href="{{ asset('template/assets/img/favicon.png') }}" rel="icon"> --}}
+        {{-- <link href="{{ asset('template/assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon"> --}}
+
+        <!-- Google Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+        <link href="{{ asset('template/assets/vendor/animate.css/animate.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('template/assets/vendor/aos/aos.css') }}" rel="stylesheet">
+        <link href="{{ asset('template/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('template/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+        <link href="{{ asset('template/assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('template/assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
+        <link href="{{ asset('template/assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
+
   </head>
   <body>
     @include('partials.navbar')
     @yield('konten')
+    @include('partials.footer')
+
+
+
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    
+    <!-- Vendor JS Files -->
+    <script src="{{ asset('template/assets/vendor/purecounter/purecounter.js') }}"></script>
+    <script src="{{ asset('template/assets/vendor/aos/aos.js') }}"></script>
+    <script src="{{ asset('template/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('template/assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
+    <script src="{{ asset('template/assets/vendor/php-email-form/validate.js') }}"></script>
+    {{-- @stack('script') --}}
+
+    <!-- Template Main JS File -->
+    <script src="{{ asset('template/assets/js/main.js') }}"></script>
   </body>
 </html>
