616f49571226d9fb61c8edb9ad59b68d2b6d0db7
devfaus <raafiasta@gmail.com>
2022-04-04 12:37:48 +0700

    bismillah

--- IREPORT/resources/views/about.blade.php
+++ IREPORT/resources/views/about.blade.php
@@ -1,7 +1,26 @@
 @extends('layouts.master')
 @section('konten')
 
-
-About page
+    &nbsp;
+    <div class="d-flex flex-row justify-content-center align-items-center">
+            <img src="{{ URL::to('logoIREPORT_full.png') }}" 
+                alt="iReport" 
+                class="img-fluid"
+                style="width: 280px;">
+        </div>
+    </div>
+    &nbsp;
+    &nbsp;
+    <div style="padding-left: 200px;
+                padding-right: 200px;
+                padding-top:;
+                padding-bottom: 202px;" class="d-flex flex-row justify-content-center align-items-center">
+        <div class="col">
+            <img src="{{ URL::to('CONSTRUCTION.jpg') }}" alt="Construction" class="img-fluid">
+        </div>
+        <div class="col">
+            <h4>Website iReport ini memfasilitasi masyarakat Kabupaten Mojokerto Jawa Timur dalam memberikan informasi kerusakan fasilitas publik yang dilewatinya, seperti jalan raya, trotoar, penerangan jalan, maupun saluran air. Ketika masyarakat menemukan fasilitas publik yang perlu diperbaiki, maka masyarakat hanya perlu untuk memfoto keadaan yang perlu diperbaiki, kemudian mengirimkannya pada halaman yang tersedia di website iReport.</h4>
+        </div>
+    </div>
 
 @endsection
