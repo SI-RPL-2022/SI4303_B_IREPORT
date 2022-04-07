2b8f826638e2584d9469830476c6d2bca5c79d6e
Kamilianurunnadhifah <92259786+Kamilianurunnadhifah@users.noreply.github.com>
2022-04-07 13:04:37 +0700

    Memperbaiki button logout dan routenya ref #4

--- IREPORT/resources/views/partials/navbar.blade.php
+++ IREPORT/resources/views/partials/navbar.blade.php
@@ -20,6 +20,13 @@
             </ul>
 
             <div class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    {{-- <i class="bi bi-person-circle"></i> --}}
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                    </svg>
+                </a>
                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                     {{-- <li><a class="dropdown-item" href="#!">Hai User</a></li> --}}
                     {{-- <li><hr class="dropdown-divider" /></li> --}}
