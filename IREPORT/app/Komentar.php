e7c56037a99d5f9f5695748b146bec2186dc10e1
irfan-github <muh.irfan201520169f@gmail.com>
2022-06-26 21:57:44 +0700

    fitur komen laporan ref #52

---
+++ IREPORT/app/Komentar.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Komentar extends Model
+{
+    protected $table = 'komentar';
+    protected $guarded = [];
+    // public $timestamps = false;
+}
